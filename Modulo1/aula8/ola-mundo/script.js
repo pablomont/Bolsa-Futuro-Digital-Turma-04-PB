e2c669be3d22a5fe791411108a1c130c8822df9c
Mayara Moura <mayarpk07@gmail.com>
2025-09-01 19:54:56 +0000

    atividade imc

---
+++ Modulo1/aula8/ola-mundo/script.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+let peso = prompt("Informe seu peso: ")
+let altura = prompt("Informe sua altura: ")
+let imc = peso / (altura * altura)
+console.log("O resultado do seu IMC é: ", imc)
+
+if(imc>25)
+{
+    console.log("IMC acima do normal")
+} else {
+    console.log("IMC normal")
+}
