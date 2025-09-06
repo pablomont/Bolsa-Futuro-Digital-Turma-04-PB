7ed4dbabef194d1be6daf16e988e1d44de627431
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-09-06 19:23:31 +0000

    exercício sobre swtich

---
+++ Modulo1/aula09/aula.js
@@ -0,0 +1,55 @@
+/*var a
+if(a){
+    console.log("entrou no if")
+    }else{
+        console.log("entrou no else")
+        }
+*/
+/*
+var corFavorita = 'Azul';
+if (corFavorita === "azul " || corFavorita==="verde"){
+    console.log("Olhe para o céu ou para árvore")
+}
+
+switch (corFavorita){
+    case 'Azul':
+    case 'verde':
+        console.log('Olhe para o céu.')
+        break;
+    case 'Vermelho':
+        console.log('Olhe para as rosas')
+        break;
+    case 'Amarelo':
+        console.log('Olhe para o sol')
+        break;
+    default:
+        console.log("feche os olhos")
+    }*/
+
+var dia=1;
+
+switch (dia){
+        case 1:
+            console.log("Dia útil")
+            break;
+        case 2:
+            console.log("Dia útil")
+            break;
+        case 3:
+            console.log("Dia útil")
+            break;
+        case 4:
+            console.log("Dia útil")
+            break;
+        case 5:
+            console.log("Dia útil")
+            break;
+        case 6:
+            console.log("Final de semana")
+            break;
+        case 7:
+            console.log("Final de semana")
+            break;
+        default: 
+            console.log("Não encontrado.")
+}
