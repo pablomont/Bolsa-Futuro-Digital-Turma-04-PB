0a1808ccfb66e7e8b10af7cac03832cd376fe353
Isadora Bessa <isadora.rbessa@gmail.com>
2025-09-06 19:26:22 +0000

    Dia de semana

---
+++ Modulo1/aula9-isadorabss/dia-da-semana.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')();
+
+let dia = prompt("Informe o dia da semana: ");
+
+switch(dia){
+    case '1':
+        console.log("Final de semana");
+        break
+    case '2':
+        console.log("Dia útil");
+        break
+    case '3':
+        console.log("Dia útil");
+        break
+    case '4':
+        console.log("Dia útil");
+        break
+    case '5':
+        console.log("Dia útil");
+        break
+    case '6':
+        console.log("Dia útil");
+        break
+    case '7':
+        console.log("Final de semana");
+        break
+}
+process.exit();
