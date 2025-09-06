74fd04e971745662a4278bb7bba45cb8f6f59b4e
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-09-06 14:17:42 +0000

    Última refatoração do exercício 4

--- Modulo1/lista04/imc/calculadorasimples.js
+++ Modulo1/lista04/imc/calculadorasimples.js
@@ -1,15 +1,27 @@
 const prompt=require('prompt-sync')(); 
 let numero1 = Number(prompt("Informe o primeiro número: "))
 let numero2 = Number(prompt("Informe o segundo número: "))
+if (isNaN(numero1)){
+    console.log("Erro {} não é um número válido", numero1)
+    process.exit();
+}
+if(isNaN(numero2)){
+    console.log("Erro: {} Não é um número válido")
+    process.exit();
+}
 let operador= prompt("Digite a operação (+,-,*, ou /): ")
 let resultado
 
 if (operador === '+'){
-    resultado = numero1 + numero2
 } else if(operador === '-'){
     resultado = numero1 - numero2
 } else if(operador === '/'){
+    if(numero2===0){
+        console.log("Erro: Não é possível dividir por zero.")
+
+    }else{
     resultado = numero1 / numero2
+    }
 } else if (operador === '*'){
     resultado = numero1 * numero2
 } else {
@@ -17,4 +29,3 @@ if (operador === '+'){
 }
 
 console.log("O resultado da operação escolhida foi:", resultado)
-
