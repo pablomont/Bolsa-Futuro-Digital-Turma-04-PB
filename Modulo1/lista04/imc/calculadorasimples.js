8420ad372cb3fcf773fe90be5db78250431f6af1
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-09-06 13:59:51 +0000

    correção

--- Modulo1/lista04/imc/calculadorasimples.js
+++ Modulo1/lista04/imc/calculadorasimples.js
@@ -1,7 +1,7 @@
 const prompt=require('prompt-sync')(); 
 let numero1 = Number(prompt("Informe o primeiro número: "))
 let numero2 = Number(prompt("Informe o segundo número: "))
-let operador= prompt("Informe a operação a ser feita: ")
+let operador= prompt("Digite a operação (+,-,*, ou /): ")
 let resultado
 
 if (operador === '+'){
@@ -13,8 +13,8 @@ if (operador === '+'){
 } else if (operador === '*'){
     resultado = numero1 * numero2
 } else {
-    console.log("Operação inválida. ")
+    console.log("Operação inválida. Por favor,  use +, -, * ou /.")
 }
 
 console.log("O resultado da operação escolhida foi:", resultado)
-process.exit();
+
