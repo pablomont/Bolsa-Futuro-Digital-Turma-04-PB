d99378559b6e4629c3c38d13d6e6d847c800e75e
gabydiniz99 <gabriellasantosdiniz@gmail.com>
2025-09-06 19:45:32 +0000

    Atualização aula 09

---
+++ Modulo1/aula-09/SwitchCase.js
@@ -0,0 +1,28 @@
+var diaDaSemana = "Segunda-Feira"
+
+switch (diaDaSemana){}
+    case "Segunda-Feira"
+    console.log("Dia útil")
+    break;
+    case "Terça-Feira"
+    console.log("Dia útil")
+    break;
+    case "Quarta-Feira"
+    console.log("Dia útil")
+    break;
+    case "Quinta-Feira"
+    console.log("Dia útil")
+    break;
+    case "Sexta-Feira"
+    console.log("Dia útil")
+    break;
+    case "Sábado"
+    console.log("Final de Semana")
+    break;
+     case "Domingo"
+    console.log("Final de Semana")
+    break;
+
+
+
+}
---
+++ Modulo1/aula-09/aula9.js
@@ -0,0 +1,20 @@
+var corFavorita ="azul"
+
+
+switch (corFavorita){
+    
+    case "azul":
+    console.log("Olhe para o céu")
+    break;
+    case "vermelho":
+    console.log("Olhe para rosas")
+    break;
+    case "amarelo":
+    console.log("Olhe para o sol")
+    break;
+    default:
+    console.log("feche os olhos")
+    break;
+
+
+}
