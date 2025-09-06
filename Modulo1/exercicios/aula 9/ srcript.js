8b7a3c34ca04549936c978c059c27ff68aea0afc
Melquisedeque-00 <melquisedequemedeiros31@gmail.com>
2025-09-06 19:38:00 +0000

    aula 9

---
+++ Modulo1/exercicios/aula 9/ srcript.js
@@ -0,0 +1,14 @@
+var corFavorita = "azul"
+
+switch (corFavorita)  {
+    case "azul" :
+        console.log("olhe para o ceu ")
+        break;
+    case "vermelho" :  
+        console.log ( "olhe para as rosas")  
+        break ;
+    case "amarelo":
+        console.log ("olhe para o sol")
+        break;
+
+}
---
+++ Modulo1/exercicios/aula 9/script.js
@@ -0,0 +1,20 @@
+var dia =7;
+
+switch (dia) {
+    case 1 :
+    case 2 :
+    case 3 :
+    case 4 :
+    case 5 :
+        console.log ("Dia util");
+        break;
+
+
+    case 6 :
+    case 7 :
+        console.log ("Final de semana");
+        break;
+
+        default:
+            console.log ("Dia invalido!  Digite um número de 1 a 7.");
+}
