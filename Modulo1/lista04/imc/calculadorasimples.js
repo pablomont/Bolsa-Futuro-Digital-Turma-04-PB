ec58aacf2b4967bef8e7c6fee41a213bfbc26602
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-09-06 13:52:04 +0000

    exercício calculadora simples

--- Modulo1/lista04/imc/calculadorasimples.js
+++ Modulo1/lista04/imc/calculadorasimples.js
@@ -1,19 +1,20 @@
 const prompt=require('prompt-sync')(); 
-let numero1=prompt("Informe o primeiro número: ")
-let numero2=prompt("Informe o segundo número: ")
-let operacao=prompt("Informe a operação a ser feita: ")
+let numero1 = Number(prompt("Informe o primeiro número: "))
+let numero2 = Number(prompt("Informe o segundo número: "))
+let operador= prompt("Informe a operação a ser feita: ")
 let resultado
 
-if (operacao === '+'){
-    resultado = numero1+numero2
-}else if(operacao === '-'){
-    resultado = numero1-numero2
-}else if(operacao === '/'){
-    resultado = numero1/numero2
-}else if (operacao === '*'){
-    resultado = numero1*numero2
-}else{
+if (operador === '+'){
+    resultado = numero1 + numero2
+} else if(operador === '-'){
+    resultado = numero1 - numero2
+} else if(operador === '/'){
+    resultado = numero1 / numero2
+} else if (operador === '*'){
+    resultado = numero1 * numero2
+} else {
     console.log("Operação inválida. ")
 }
-processo.exit();
-console.log("O resultado da operação escolhida foi: ", resultado)
+
+console.log("O resultado da operação escolhida foi:", resultado)
+process.exit();
