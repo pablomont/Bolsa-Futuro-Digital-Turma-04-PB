5fee11cbffba7b033e14c041d1f00e82d52bad6d
Héllida Louize <h.louize@aluno.uepb.edu.br>
2025-09-06 19:26:29 +0000

    Switch

---
+++ Modulo1/Exercícios/switch.js
@@ -0,0 +1,31 @@
+const prompt = require('prompt-sync')();
+
+let dia = prompt("Informe o dia da semana: ")
+switch (dia) {
+    case ("1"):
+        console.log("Dia Útil")
+        break
+
+    case ("2"):
+        console.log("Dia Útil")
+        break
+
+    case ("3"):
+        console.log("Dia Útil")
+        break
+
+    case ("4"):
+        console.log("Dia Útil")
+        break
+
+    case ("5"):
+        console.log("Dia Útil")
+        break
+    case ("6"):
+        console.log("Final de semana")
+        break
+    case ("7"):
+        console.log("Final de semana")
+        break
+}
+process.exit();
