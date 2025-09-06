f2632e476f8748ecd523647d50017714e84a0260
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-09-06 10:39:52 -0300

    Update calculadorasimples.js
    Finalizado

--- Modulo1/lista04/imc/calculadorasimples.js
+++ Modulo1/lista04/imc/calculadorasimples.js
@@ -4,15 +4,16 @@ let numero2=prompt("Informe o segundo número: ")
 let operacao=prompt("Informe a operação a ser feita: ")
 let resultado
 
-if (operacao == '+'){
+if (operacao === '+'){
     resultado = numero1+numero2
-}if(operacao == '-'){
+}else if(operacao === '-'){
     resultado = numero1-numero2
-}if(operacao == '/'){
+}else if(operacao === '/'){
     resultado = numero1/numero2
-}if (operacao == '*'){
+}else if (operacao === '*'){
     resultado = numero1*numero2
 }else{
     console.log("Operação inválida. ")
 }
+processo.exit();
 console.log("O resultado da operação escolhida foi: ", resultado)
