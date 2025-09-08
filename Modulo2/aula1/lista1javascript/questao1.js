3189d343f00fc2d437dd19cf9b2df250facc3605
Mayara Moura <mayarpk07@gmail.com>
2025-09-08 19:47:59 +0000

    questão 1 lista 1 javascript

--- Modulo2/aula1/lista1javascript/questao1.js
+++ Modulo2/aula1/lista1javascript/questao1.js
@@ -1,7 +1,15 @@
-const prompt = require('prompt-sync') ();
+/*const prompt = require('prompt-sync') ();
 var nome = passarNome()
 console.log(`Olá ${nome}, bem-vindo ao mundo da programação!`)
 
 function passarNome(){
     return prompt("Informe o seu nome: ")
+} */
+
+const prompt = require('prompt-sync') ();
+var nome = prompt("digite seu nome: ")
+
+function saudacao(){
+    return console.log(`Olá ${nome}, bem-vindo ao mundo da programação!`)
 } 
+saudacao(nome)
