2779b7fb8733460ddb283798bfdef741cb97a050
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-09-09 02:07:53 +0000

    Correção no código exercício5

--- Modulo2/aula10/lista1_js/ex05.js
+++ Modulo2/aula10/lista1_js/ex05.js
@@ -1,19 +1,22 @@
 const prompt = require('prompt-sync')();
 
-let nota1=prompt("Informe a primeira nota: ")
-let nota2=prompt("Informe a segunda nota: ")
-let nota3= prompt("Informe a terceira nota: ")
+var nota1=prompt("Informe a primeira nota: ")
+var nota2=prompt("Informe a segunda nota: ")
+var nota3= prompt("Informe a terceira nota: ")
+var numero1 = Number(nota1)
+var numero2 = Number(nota2)
+var numero3 = Number(nota3)
 
-console.log(media(nota1, nota2, nota3))
+console.log(media(numero1, numero2, numero3))
 
 function media(nota1, nota2, nota3){
     media = (nota1+nota2+nota3)/3;
     
     if (media>=7){
-        return "Aprovado."
+        return "Aprovado"
     }else if(media<=6.99 && media>=5){
-        return "Recuperação."
+        return "Recuperação"
     }else{
-        return "Reprovado."
+        return "Reprovado"
     }
 }
