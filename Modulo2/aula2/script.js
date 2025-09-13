a553413ab0704adc6d47901b72b797d939fb446d
Mayara Moura <mayarpk07@gmail.com>
2025-09-13 18:43:33 +0000

    aula 2

---
+++ Modulo2/aula2/script.js
@@ -0,0 +1,30 @@
+function printResult(){
+    console.log("entrou em printResult")
+    const res = multiply(3,2)
+    console.log("entrou em printResult")
+    console.log(res)
+}
+
+function multiply(num1,num2){
+    console.log("entrou em multiply")
+    return num1*num2
+}
+printResult()
+
+//function terceiraIdade(idade){
+//    return "minha idade é ..."
+//}
+
+//if (condicao){
+//    let a = ""
+//}
+//a = "asdada"
+
+//if (condicao){
+ //   var a = ""
+//}
+//a = "asdada"
+
+//function nomeFuncao(parametro){
+//    a = ""
+//}
