2afab112e12f8b569a8b82fa06ff60738094525d
Emillygvitoria <emillygomes1018@gmail.com>
2025-09-13 00:04:17 +0000

    atualização

--- Modulo2/Lista1/questão2.js
+++ Modulo2/Lista1/questão2.js
@@ -1,7 +1,15 @@
-
-function ehTruthy(valor) {
-
-    return valor;
+function isTruthy(valor) {
+  return Boolean(valor);
 }
-console.log(ehTruthy(0));
-console.log(ehTruthy("teste"));
+
+console.log(isTruthy(true));     
+console.log(isTruthy(1));        
+console.log(isTruthy("hello"));  
+console.log(isTruthy({}));       
+console.log(isTruthy([]));       
+console.log(isTruthy(false));    
+console.log(isTruthy(0));        
+console.log(isTruthy(""));       
+console.log(isTruthy(null));     
+console.log(isTruthy(undefined)); 
+console.log(isTruthy(NaN));      
