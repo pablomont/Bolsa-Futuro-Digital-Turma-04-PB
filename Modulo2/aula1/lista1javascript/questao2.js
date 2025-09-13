beeda16bbed4f2425143400325eb1435eccc3642
Mayara Moura <mayarpk07@gmail.com>
2025-09-13 18:41:13 +0000

    questão 2

--- Modulo2/aula1/lista1javascript/questao2.js
+++ Modulo2/aula1/lista1javascript/questao2.js
@@ -1,1 +1,15 @@
-          
+ function isTruthy(valor) {
+  if (valor) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isTruthy(false));        
+console.log(isTruthy(0));
+console.log(isTruthy(NaN));                                  
+console.log(isTruthy(null));         
+console.log(isTruthy(undefined));    
+console.log(isTruthy(""));
+console.log(isTruthy("hello"));            
