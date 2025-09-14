4663f205ae44757f0d196e79728f664a93cac05b
Mayara Moura <mayarpk07@gmail.com>
2025-09-14 23:53:28 +0000

    questão 3

--- Modulo2/aula1/lista1javascript/questao3.js
+++ Modulo2/aula1/lista1javascript/questao3.js
@@ -1,6 +1,19 @@
-function numPar(num){
+/*function numPar(num){
     return num%2 === 0;
 }
 
 console.log(numPar(4));
 console.log(numPar(7));
+*/
+// Ver se é par
+
+function numeroPar(num) {
+  if (num % 2 === 0) {
+    console.log(`O número ${num} é par`);
+  } else {
+    console.log(`O número ${num} é ímpar`);
+  }
+}
+
+numeroPar(4);  
+numeroPar(7);  
