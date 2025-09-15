830b706a5437f34ff8ccbfaef2af7b2cbaa4b240
Mayara Moura <mayarpk07@gmail.com>
2025-09-15 18:03:43 +0000

    converter horas em minutos

---
+++ Modulo2/aula3/horas.js
@@ -0,0 +1,21 @@
+/*function convertHoursToMinutes(hours) {
+        return hours * 60;
+}
+
+console.log(convertHoursToMinutes(2)); */
+
+const prompt = require("prompt-sync")();
+
+function convertHoursToMinutes(hours) {
+    return hours * 60;
+}
+
+const hoursEntrada = prompt("Digite a quantidade de horas: ");
+const hours = Number(hoursEntrada);
+
+if (isNaN(hours)) {
+    console.log("Por favor, insira um número válido.");
+} else {
+    const minutos = convertHoursToMinutes(hours);
+    console.log(`${minutos} minutos.`);
+}
