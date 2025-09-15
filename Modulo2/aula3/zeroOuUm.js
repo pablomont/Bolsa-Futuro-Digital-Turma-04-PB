4c0ee45d4339250b4b15972b55926c1ad56a1b63
Mayara Moura <mayarpk07@gmail.com>
2025-09-15 18:34:43 +0000

    jogo: zero ou um

---
+++ Modulo2/aula3/zeroOuUm.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+const A = parseInt(prompt("A, digite 0 ou 1: "));
+const B = parseInt(prompt("B, digite 0 ou 1: "));
+const C = parseInt(prompt("C, digite 0 ou 1: "));
+
+if (A !== B && A !== C) {
+    console.log('A');
+} else if (B !== A && B !== C) {
+    console.log('B');
+} else if (C !== A && C !== B) {
+    console.log('C');
+} else {
+    console.log('*');
+} 
