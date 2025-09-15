f5eceb13eca80f0a09c8882137d4af3e572ae30e
SuelleMaciel <suellemaciel7@gmail.com>
2025-09-15 14:40:47 -0300

    Updating.

--- Modulo2/Lista1Refatorada/questao1.js
+++ Modulo2/Lista1Refatorada/questao1.js
@@ -31,17 +31,37 @@ function verificarSituacao(media, notaAprovacao, notaRecuperacao) {
   }
 }
 
+function verificarConceito(media) {
+  if (media >= 9.0 && media <= 10.0) {
+    return "A";
+  } else if (media >= 7.0 && media <= 8.9) {
+    return "B";
+  } else if (media >= 5.0 && media <= 6.9) {
+    return "C";
+  } else if (media >= 3.0 && media <= 4.9) {
+    return "D";
+  } else if (media >= 0.0 && media <= 2.9) {
+    return "F";
+  } else {
+    return "NAO SE APLICA.";
+  }
+}
+
 //4ª
-function exibirResultado(media, situacao) {
-  console.log("\n=== MODO DE EXIBIÇÃO ===\n1- SIMPLES\n2- DETALHADO");
-  const escolha = prompt("Escolha o modo de exibição (1 ou 2): ");
+function exibirResultado(media, situacao, conceito) {
+  console.log("\n=== MODO DE EXIBIÇÃO ===\n1- SIMPLES\n2- DETALHADO\n3-CONCEITO");
+  const escolha = prompt("Escolha o modo de exibição (1, 2 ou 3): ");
 
   if (escolha === "1") {
     console.log(`\nSituação: ${situacao}`);
-  } else if (escolha === "2") {
+    } else if (escolha === "2") {
     console.log(`\nMédia: ${media}`);
     console.log(`Situação: ${situacao}`);
-  } else {
+    }
+    else if (escolha === "3"){
+    console.log(`Conceito - ${conceito}`)
+    }
+    else {
     console.log("Opção inválida.");
   }
 }
@@ -51,9 +71,12 @@ function main() {
   const media = calcularMedia()
   
   const criterios = obterCriterios();
+  const conceito = verificarConceito(media)
   const situacao = verificarSituacao(media, criterios.notaAprovacao, criterios.notaRecuperacao);
-  
-  exibirResultado(media, situacao);
+  exibirResultado(media, situacao, conceito);
+  console.log("OEPRAÇÃO FINALIZADA\n")
 }
 
-main();
+main()
+main()
+main()
