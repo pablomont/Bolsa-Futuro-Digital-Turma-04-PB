457e20b040d75785b4bd1b2a4b51ee95063c11b0
SuelleMaciel <suellemaciel7@gmail.com>
2025-09-20 15:37:48 -0300

    Testes com objetos.

---
+++ Modulo2/aula-12/objeto1.js
@@ -0,0 +1,9 @@
+var carro = {
+    valor: "R$200.000",
+    modelo: "T-cross",
+    portas: "4 portas"
+
+}
+
+console.log(carro.valor, ",", carro.modelo, ",", carro.portas)
+
---
+++ Modulo2/aula-12/objeto2.js
@@ -0,0 +1,32 @@
+var dados = {
+    nome: "Suelle",
+    sobrenome: "Maciel",
+
+    mostrarDados: function(){
+        return this.nome + " " + this.sobrenome
+    }
+}
+
+console.log(dados.mostrarDados())
+
+var carro = {
+    preco: 10000,
+    portas: 4,
+    marca: "Audi"
+}
+
+console.log("Valor antes da alteração:", carro.preco)
+carro.preco = 30000
+console.log("Valor depois da alteração:", carro.preco)
+
+var cachorro = {
+    raca: "labrador",
+    cor: "preto",
+    idade: 10,
+
+    viuHomem: function latir(){
+        return "auau"
+    }
+}
+
+console.log(cachorro.viuHomem())
