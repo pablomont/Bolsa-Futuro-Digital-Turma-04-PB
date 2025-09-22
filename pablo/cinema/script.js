824d884b065bb7103a957a78cc45cbde16c4b85a
Pablo Monteiro <paabloms@live.com>
2025-09-22 15:24:53 +0000

    resolvendo questao 1

---
+++ pablo/cinema/script.js
@@ -0,0 +1,18 @@
+let altura = 1.2
+console.log(altura.toFixed())
+
+const texto = "tema-claro.css";
+console.log(texto.includes("claro")); // true
+console.log(texto.includes("escuro")); // false
+
+
+// Execute ao carregar a página
+document.addEventListener('DOMContentLoaded', carregarTema);
+
+function carregarTema() {
+
+    // 1. Leia o cookie
+
+    // 2. Se tema=escuro, altere para tema-escuro.css
+
+}
