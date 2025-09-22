5dd758d86ead1f1f58317c33a1b4c165d01684bb
tulio741 <62000374+tulio741@users.noreply.github.com>
2025-09-22 17:32:04 +0000

    add aula4

---
+++ Modulo2/aula4/aula4.js
@@ -0,0 +1,18 @@
+var aluno = {
+    nome: "Túlio",
+    sobrenome: "Martins",
+    idade: "33",
+}
+
+console.log(aluno.nome +" "+ aluno.sobrenome)
+
+var carro ={
+    preco: 1000,
+    portas: 4,
+    marca: "Audi",
+    
+}
+
+let novoValor = carro.preco = "novo valor: 3000"
+
+console.log (novoValor)
---
+++ Modulo2/aula4/scoreboard/scoreboard.js
@@ -0,0 +1,8 @@
+var score1 = 0;
+let btn = document.getElementById("add1");
+btn.onclick = function() {
+    score1 + 1;
+    alert("score1");
+    let tdscore1 = document.getElementById("score1");
+    tdscore1
+}
