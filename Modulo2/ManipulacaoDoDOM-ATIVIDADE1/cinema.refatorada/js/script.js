51e1d9b3bced5463f2d9ad2b381aa3952ce8aab5
LaraFernandesz <lara.marina@aluno.uepb.edu.br>
2025-09-26 10:14:02 -0300

    Questao 3 resolvida

--- Modulo2/ManipulacaoDoDOM-ATIVIDADE1/cinema.refatorada/js/script.js
+++ Modulo2/ManipulacaoDoDOM-ATIVIDADE1/cinema.refatorada/js/script.js
@@ -3,8 +3,23 @@ function alterarTema() {
     const hrefAtual = link.getAttribute('href');
 
     if (hrefAtual.includes('dark')) {
-        link.setAttribute('href', 'css/estilo.css');
+        link.setAttribute('href', hrefAtual.replace('estilo-dark.css', 'estilo.css'));
+        localStorage.setItem('theme', 'light');
     } else {
-        link.setAttribute('href', 'css/estilo-dark.css');
+        link.setAttribute('href', hrefAtual.replace('estilo.css', 'estilo-dark.css'));
+        localStorage.setItem('theme', 'dark');
     }
 }
+
+function carregarTema() {
+    const link = document.querySelector('link[rel="stylesheet"]');
+    const temaSalvo = localStorage.getItem('theme');
+
+    if(temaSalvo === 'dark') {
+        link.setAttribute('href', link.getAttribute('href').replace('estilo.css', 'estilo-dark.css'));
+    } else {
+        link.setAttribute('href', link.getAttribute('href').replace('estilo-dark.css', 'estilo.css'));
+    }       
+}
+
+document.addEventListener('DOMContentLoaded', carregarTema);
