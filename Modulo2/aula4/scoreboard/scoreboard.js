5d3155947a577c7d8edafb67e759ef766d9e3c93
tulio741 <62000374+tulio741@users.noreply.github.com>
2025-09-26 12:04:41 +0000

    adiciona arquivo aula4.js com definição de objeto aluno e carro

--- Modulo2/aula4/scoreboard/scoreboard.js
+++
@@ -1,8 +0,0 @@
-var score1 = 0;
-let btn = document.getElementById("add1");
-btn.onclick = function() {
-    score1 + 1;
-    alert("score1");
-    let tdscore1 = document.getElementById("score1");
-    tdscore1
-}
