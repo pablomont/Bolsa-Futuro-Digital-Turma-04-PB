d9af3d7d33b61d8ad8f97e75ebb2751012ac5fd9
SuelleMaciel <suellemaciel7@gmail.com>
2025-09-27 16:29:10 -0300

    Exercício com arrays

---
+++ atividadesTeste/exercicioArrays.js
@@ -0,0 +1,15 @@
+var copas = [1959, 1962, 1994, 2002]
+
+for (let i = 0; i < copas.length; i++){
+    console.log(`O Brasil ganhou a copa de ${copas[i]}`)
+}
+
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melancia']
+
+ultimaFruta = frutas[frutas.length - 1]
+
+console.log(ultimaFruta)
+
+for (let i = 0; i < frutas.length; i++){
+    console.log(`Fruta nº ${i}: ${frutas[i]}`)
+}
