19cb73e4b2983cbbb0bf586caaaa3d25a8156a8f
Horlanlacerda <horlansilva56@gmail.com>
2025-09-27 19:39:49 -0300

    Adição da validação + process.exit() na questão da Média.js (Módulo1_Aula08)

--- Modulo1/Aula08_Horlan_Exercícios/Media.js
+++ Modulo1/Aula08_Horlan_Exercícios/Media.js
@@ -2,7 +2,18 @@
 const prompt = require('prompt-sync')()
 
 let nota1 = parseFloat(prompt('Informe sua primeira nota: '));
+
+if(isNaN(nota1) || nota1 <= 0 || nota1 > 10){
+    console.log('Valor inválido.');
+    process.exit();
+}
+
 let nota2 = parseFloat(prompt('Informe sua segunda nota: '));
+
+if(isNaN(nota2) || nota2 <= 0 || nota2 > 10){
+    console.log('Valor inválido.');
+    process.exit();
+}
 
 let media  = (nota1 + nota2) / 2;
 
