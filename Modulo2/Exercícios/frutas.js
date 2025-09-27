01d19d0438e53220b6de9f6fadfc25d90bdfe600
Héllida Louize <h.louize@aluno.uepb.edu.br>
2025-09-27 19:58:59 +0000

    frutas

---
+++ Modulo2/Exercícios/frutas.js
@@ -0,0 +1,5 @@
+var frutas = ['Banana', 'Maça', 'Pera', 'Uva', 'Melancia']
+const index = frutas.indexOf("Pera");
+for (let i = 0; i < index; i++) {
+    console.log(frutas[i])}
+    
---
+++ Modulo2/Exercícios/frutasconst.js
@@ -0,0 +1,3 @@
+var frutas = ['Banana', 'Maça', 'Pera', 'Uva', 'Melancia']
+ultima = frutas[(frutas.length - 1)]
+console.log(ultima)
