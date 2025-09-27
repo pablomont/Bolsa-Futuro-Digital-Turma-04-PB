be37db2711314d2fa99434c857a6dc6b68551ae1
Héllida Louize <h.louize@aluno.uepb.edu.br>
2025-09-27 19:12:26 +0000

    IMC com do while

--- Modulo1/Exercícios/IMC.js
+++ Modulo1/Exercícios/IMC.js
@@ -1,17 +1,35 @@
 const prompt = require('prompt-sync')();
 
-let peso = prompt("Digite seu peso (em kg): ")
-pesonum = parseFloat(peso)
-let Altura = prompt("Digite sua altura (em metros): ")
-alturanum = parseFloat(Altura)
 
-imc = pesonum / (alturanum * alturanum)
-console.log("Seu imc é:", imc)
-
-if (imc > 25) {
-    console.log("IMC ACIMA DO NORMAL")
+function valorInvalido(peso, altura) {
+    return isNaN(peso) || isNaN(altura) || peso < 0 || altura < 0;
 }
-else {
+
+let peso;
+let altura;
+
+
+do {
+    peso = parseFloat(prompt("Digite seu peso (em kg): "));
+    altura = parseFloat(prompt("Digite sua altura (em metros): "));
+
+    if (valorInvalido(peso, altura)) {
+        console.log("Valor inválido. 🙄 Tente novamente.");
+    }
+
+} while (valorInvalido(peso, altura));
+
+
+const imc = peso / (altura * altura);
+
+
+if (imc > 25) 
+    console.log("IMC ACIMA DO NORMAL");
+else if (imc < 18.5)
+    console.log("IMC ABAIXO DO NORMAL")
+else 
     console.log("IMC NORMAL")
-}
+
+
+
 process.exit();
