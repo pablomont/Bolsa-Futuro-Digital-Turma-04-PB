3f15c97b5cc62389cfb44232acba28e66268d904
LucianoSilva <113153213+I-Zin-I@users.noreply.github.com>
2025-09-27 18:04:17 +0000

    novo 7

---
+++ Modulo 2 - Luciano Silva/Aula15 - javasscript 6/script.js
@@ -0,0 +1,13 @@
+let filme = [Blade runner, ]
+
+var filmeDuna = {
+    nomeFilme: duna ,
+    anoLançamento: '2819'
+}
+
+let filme =  [blade runner' , 't
+ function(filme) {return 'O filme ' = filme.anolançamento}]
+
+let exibeAnoLançamento = filmes[4]
+
+console.log(exibeAnoLançamento{filmeDuna})
