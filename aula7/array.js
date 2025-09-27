6d3044ee9614cc3981225cc5513c2e6a0cf57569
Danilo Gomes <danillogomes1902@gmail.com>
2025-09-27 19:39:52 +0000

    aula dia 27

--- aula7/array.js
+++ aula7/array.js
@@ -1,19 +1,19 @@
-//var filmeDuna = {
-//    nomeFilme: 'duna',
- //   anoLacanmento: '2019'
+var filmeDuna = {
+nomeFilme: 'duna',
+anoLacanmento: '2019'
 }
 
-// var filmes = ['blade runner', 'titanic', 'avatar', filmeDuna,
-//     function(filme) {return "O filme foi lançado em: "+filme.anoLacanmento}]
+var filmes = ['blade runner', 'titanic', 'avatar', filmeDuna,
+function(filme) {return "O filme foi lançado em: "+filme.anoLacanmento}]
 
 //var filmes = ['blade runner', 'titanic', 'avatar', filmeDuna,
 //(filme) =>"O filme foi lançado em: "+filme.anoLacanmento]
 
-// exibeAnoLancamento = filmes[4]
+exibeAnoLancamento = filmes[4]
 
-//filmes.push('interstellar')
-//console.log(filmes)
+filmes.push('interstellar')
+console.log(filmes)
 
-//console.log(exibeAnoLancamento(filmeDuna))
+console.log(exibeAnoLancamento(filmeDuna))
 
 
