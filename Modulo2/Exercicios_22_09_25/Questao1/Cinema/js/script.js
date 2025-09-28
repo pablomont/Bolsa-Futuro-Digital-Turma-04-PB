de3a0c4329e0425cdfdbf6807e4220c8de183d7d
Horlanlacerda <horlansilva56@gmail.com>
2025-09-28 11:22:45 -0300

    Resolução: Questão 1 (Exercício_22_09_25)

---
+++ Modulo2/Exercicios_22_09_25/Questao1/Cinema/js/script.js
@@ -0,0 +1,12 @@
+
+let botao = document.getElementById('btn_alterar_tema')
+
+let alterarTema = function(){
+
+    let link_tema = document.querySelector('#estilo_tema'); // O "#" foi sugestão do gemini. Estava usando sem e não deu certo. Por que?
+
+    link_tema.href = 'C:/Users/ninoe/Documents/UEPB/CURSO_FRONT/Bolsa-Futuro-Digital-Turma-04-PB/Modulo2/Exercicios_22_09_25/Questao1/Cinema/css/estilo_dark.css';
+
+}
+
+botao.onclick = alterarTema;
