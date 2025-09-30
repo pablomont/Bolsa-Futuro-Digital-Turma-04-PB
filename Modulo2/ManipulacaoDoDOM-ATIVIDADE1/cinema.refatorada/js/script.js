79a95715b625404ee06928b70b66a2356cafb00d
LaraFernandesz <lara.marina@aluno.uepb.edu.br>
2025-09-30 14:37:44 -0300

    Questao 4 - Atividade DOM 1

--- Modulo2/ManipulacaoDoDOM-ATIVIDADE1/cinema.refatorada/js/script.js
+++ Modulo2/ManipulacaoDoDOM-ATIVIDADE1/cinema.refatorada/js/script.js
@@ -1,24 +1,30 @@
-function alterarTema() {
+function alterarTema(event) {
     const link= document.querySelector('link[rel="stylesheet"]');
     const hrefAtual = link.getAttribute('href');
+    const botao = event.target
 
     if (hrefAtual.includes('dark')) {
         link.setAttribute('href', hrefAtual.replace('estilo-dark.css', 'estilo.css'));
         localStorage.setItem('theme', 'light');
+        botao.textContent = 'Ativar Modo Escuro';
     } else {
         link.setAttribute('href', hrefAtual.replace('estilo.css', 'estilo-dark.css'));
         localStorage.setItem('theme', 'dark');
+        botao.textContent = 'Ativar Modo Claro';
     }
 }
 
 function carregarTema() {
     const link = document.querySelector('link[rel="stylesheet"]');
     const temaSalvo = localStorage.getItem('theme');
+    const botao = document.querySelector('#botao-alterar-tema');
 
     if(temaSalvo === 'dark') {
         link.setAttribute('href', link.getAttribute('href').replace('estilo.css', 'estilo-dark.css'));
+        botao.textContent = "Tema Claro";
     } else {
         link.setAttribute('href', link.getAttribute('href').replace('estilo-dark.css', 'estilo.css'));
+        botao.textContent = "Tema Escuro";
     }       
 }
 
