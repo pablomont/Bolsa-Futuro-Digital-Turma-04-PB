860e2442eff2b4aa62dcdda5b5c664d79eea08c9
RaulConfessor <confessorraul@gmail.com>
2025-09-30 18:39:42 -0300

    Manipulação-DOM-Ativd.1 Q3 e Q4 resolvidas

--- Modulo2/DOM-Atividade1/Cinema-1/script.js
+++ Modulo2/DOM-Atividade1/Cinema-1/script.js
@@ -1,10 +1,29 @@
 function MudarTema(){
     let elemento = document.querySelector('link[rel="stylesheet"]');
+    var btnTema = document.getElementById('tema');
 
-    if(elemento.getAttribute('href') === '/Cinema-1/css/dark-mode.css')
+    if(elemento.getAttribute('href') === '/Cinema-1/css/dark-mode.css'){
         elemento.href = '/Cinema-1/css/style.css';
-    else    
-    elemento.href = '/Cinema-1/css/dark-mode.css'
+        localStorage.setItem('tema', elemento.href);
+        alterarBtn(btnTema,'Modo Escuro');
+
+    } else{ 
+        elemento.href = '/Cinema-1/css/dark-mode.css';
+        localStorage.setItem('tema', elemento.href);
+        alterarBtn(btnTema,'Modo Claro');
+    }
 }
 
 document.getElementById('tema').addEventListener('click', MudarTema);
+
+document.addEventListener('DOMContentLoaded', function(){
+    let temaSalvo = localStorage.getItem('tema');
+    if(temaSalvo){
+        document.querySelector('link[rel="stylesheet"]').setAttribute('href', temaSalvo);
+    }
+});
+
+function alterarBtn(botao,text){
+    botao.textContent = text;
+}
+
