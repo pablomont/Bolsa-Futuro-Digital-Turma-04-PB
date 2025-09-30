e0e383acbce2baba7f8a6e690e02574b623df047
Mariana Gonçalves <marianadesousagoncalvess@gmail.com>
2025-09-30 01:58:29 +0000

    exercicioaula06

--- Modulo1/Modulo2/aula6-Mariana/exercicio.js
+++ Modulo1/Modulo2/aula6-Mariana/exercicio.js
@@ -1,3 +1,13 @@
-let copaDoMundo = ['1959', '1962', '1970', '1994', '2002']
-for(var i = 0; i < copaDoMundo.length; i++) {
+ var copasDoBrasil = ['1959', '1962', '1970', '1994', '2002']
+for (var i = 0; i < copasDoBrasil.length; i++) {
+    console.log('O Brasil ganhou a copa de ${copaDoBrasil[i]}');
 }
+ var frutas = ['banana', 'maçã', 'pera', 'uva', 'melância'];
+ for ( var i = 0; i < frutas.lengt; i++ ) {
+    console.log(frutas[i]);
+    if (frutas[i] === 'pera') {
+        break;
+    }
+ }
+ var ultimaFruta = frutas[frutas.length - 1];
+ console.log('A ultima fruta é: ${ultimaFruta}');
