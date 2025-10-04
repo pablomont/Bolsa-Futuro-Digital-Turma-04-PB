bac18adcd9122288d4461de26bff388cd0882fa7
Mayara Moura <mayarpk07@gmail.com>
2025-10-04 13:59:16 +0000

    Manipulação DOM ativ 1 questão 2 resolvida

--- Modulo2/aula7/cinema1/js/script.js
+++ Modulo2/aula7/cinema1/js/script.js
@@ -1,4 +1,10 @@
 function alterarTema() {
     const link = document.querySelector('link');
-    link.href = 'css/estilo-dark.css';
+    const hrefAtual = link.getAttribute('href');
+
+    if (hrefAtual.includes('dark')) {
+        link.href = 'css/estilo.css';
+    } else {
+        link.href = 'css/estilo-dark.css';
+    }
 }
