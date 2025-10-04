356960493e9a144a76a74a88f1c0ec1ec8075a95
Mayara Moura <mayarpk07@gmail.com>
2025-10-04 14:15:27 +0000

    Manipulação DOM ativ 1 questão 3 resolvida

--- Modulo2/aula7/cinema1/js/script.js
+++ Modulo2/aula7/cinema1/js/script.js
@@ -3,8 +3,28 @@ function alterarTema() {
     const hrefAtual = link.getAttribute('href');
 
     if (hrefAtual.includes('dark')) {
-        link.href = 'css/estilo.css';
+        link.href = link.href.replace('estilo-dark.css', 'estilo.css');
+        localStorage.setItem('theme', 'estilo.css');
     } else {
-        link.href = 'css/estilo-dark.css';
+        link.href = link.href.replace('estilo.css', 'estilo-dark.css');
+        localStorage.setItem('theme', 'estilo-dark.css');
     }
 }
+
+function carregarTema() {
+    const temaSalvo = localStorage.getItem('theme');
+    const link = document.querySelector('link');
+
+    if (temaSalvo && !link.href.includes(temaSalvo)) {
+        // Corrige caminho 
+        let novoHref = temaSalvo;
+        if (window.location.pathname.includes('estados')) {
+            novoHref = '../css/' + temaSalvo;
+        } else {
+            novoHref = 'css/' + temaSalvo;
+        }
+        link.href = novoHref;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', carregarTema);
