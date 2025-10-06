8ed200069d134c188c31aaa3ae81a9b2852aecfb
SuelleMaciel <suellemaciel7@gmail.com>
2025-10-06 10:05:04 -0300

    Atividades feitas em sala para praticar

---
+++ atividadesTeste/calculos/calcularImc.js
@@ -0,0 +1,26 @@
+document.getElementById(peso)
+
+const prompt = require("prompt-sync") ();
+
+function calcularIMC(peso, altura){
+    return imc = peso / (altura * altura);
+}
+
+do{
+    peso = parseFloat(prompt("Informe seu peso:"))
+    if((isNaN(peso) || peso < 0)){
+        console.log("Entrada inválida.")
+    }
+}
+while (isNaN(peso) || peso < 0)
+
+let altura = parseFloat(prompt("Digite a altura: "));
+
+imc = calcularIMC(peso,altura);
+
+if (imc >= 25){
+    console.log("IMC acima do normal.")
+}
+else{
+    console.log("IMC normal")
+}
