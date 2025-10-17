e498087fc491ae72151825636df0d3a2e3b73627
lucas <lucasfernandolf@hotmail.com>
2025-10-17 17:58:09 -0300

    questao1 resolve

--- Modulo3/Aula1-TypeScript/hello.js
+++
@@ -1,5 +0,0 @@
-function greet(name) {
-    return "hello, ".concat(name, "!");
-}
-var message = greet("world");
-console.log(message);
--- Modulo3/Aula1-TypeScript/hello.ts
+++
@@ -1,5 +0,0 @@
-function greet(name: string): string{
-    return `hello, ${name}!`;
-}
-const message: string = greet("world")
-console.log(message);
