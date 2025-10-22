c9cefe45a5b14d2c7a310abb5cb37717e828c35a
Mayara Moura <mayarpk07@gmail.com>
2025-10-22 00:54:45 -0300

    Implementa lógica de Pedra, Papel, Tesoura game

---
+++ Modulo3/aula3/index.ts
@@ -0,0 +1,45 @@
+const btnPedra = document.getElementById('pedra');
+const btnPapel = document.getElementById('papel');
+const btnTesoura = document.getElementById('tesoura');
+
+if (btnPedra) {
+    btnPedra.onclick = () => {
+        const escolhaComputador = obterEscolhaComputador();
+        mostrarResultado('pedra', escolhaComputador);
+    }
+}
+
+if (btnPapel) {
+    btnPapel.onclick = () => {
+        const escolhaComputador = obterEscolhaComputador();
+        mostrarResultado('papel', escolhaComputador);
+    }
+}
+
+if (btnTesoura) {
+    btnTesoura.onclick = () => {
+        const escolhaComputador = obterEscolhaComputador();
+        mostrarResultado('tesoura', escolhaComputador);
+    }
+}
+
+function obterEscolhaComputador() {
+    const opcoes = ['pedra', 'papel', 'tesoura'];
+    return opcoes[Math.floor(Math.random() * opcoes.length)];
+}
+
+function mostrarResultado(escolhaUsuario: string, escolhaComputador: string) {
+    const opcoes = `Computador: ${escolhaComputador} - Usuário: ${escolhaUsuario}`;
+    
+    if (escolhaUsuario === escolhaComputador) {
+        alert(`É um empate! \n${opcoes}`);
+    } else if (
+        (escolhaUsuario === 'pedra' && escolhaComputador === 'tesoura') ||
+        (escolhaUsuario === 'papel' && escolhaComputador === 'pedra') ||
+        (escolhaUsuario === 'tesoura' && escolhaComputador === 'papel')
+    ) {
+        alert(`Você venceu! \n${opcoes}`);
+    } else {
+        alert(`O computador venceu!\n${opcoes}`);
+    }
+}
