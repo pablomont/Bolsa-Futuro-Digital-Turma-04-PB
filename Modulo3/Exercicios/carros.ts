3d373920b687ba27519c3ef53c6280c751b75c0c
Héllida Louize <h.louize@aluno.uepb.edu.br>
2025-10-27 18:47:04 +0000

    carros

---
+++ Modulo3/Exercicios/carros.ts
@@ -0,0 +1,41 @@
+class Carro {
+    nome: string = ''
+    potencia: number = 0
+    velocidade: number = 0
+
+    acelerar(){
+        this.velocidade += this.potencia // velocidade = velocidade + potencia
+    }
+
+    frear(){
+        this.velocidade *= 0.5 // velocidade = velocidade * 0.5
+    }
+ 
+    exibirVelocidadeAtual(){
+        console.log(`A velocidade atual do carro ${this.nome} é de ${this.velocidade} km/h`)
+    }
+}
+
+const fusca = new Carro()
+fusca.nome = 'fusca 1988'
+fusca.potencia = 2
+
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.exibirVelocidadeAtual() 
+
+const ferrari = new Carro()
+ferrari.potencia = 15
+ferrari.nome = 'Ferraro GTB'
+
+ferrari.acelerar()
+ferrari.frear()
+ferrari.frear()
+ferrari.exibirVelocidadeAtual()
+
+if(ferrari.velocidade > fusca.velocidade){
+    console.log('A velocidade da ferrari é maior')
+} else {
+    console.log('A velocidade do fusca é maior')
+}
