f763c2a1c9976b1c7b10bd217e0c8c2d58b1d8e5
gabydiniz99 <gabriellasantosdiniz@gmail.com>
2025-10-27 19:55:54 +0000

    atualização da aula4

---
+++ Modulo3/Aula4/carros.ts
@@ -0,0 +1,37 @@
+import { Painel } from "./painel"
+
+class Carro {
+  
+    constructor(public nome: string, public potencia: number, public velocidade :number, 
+       public painel: Painel){}
+
+    acelerar(){
+        this.velocidade += this.potencia // velocidade = velocidade + potencia
+    }
+
+    frear(){
+        this.velocidade *= 0.5 // velocidade = velocidade * 0.5
+    }
+ 
+
+}
+
+const fusca = new Carro('fusca 1998',2,0, new Painel())
+
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.painel.exibir(fusca.velocidade)
+
+// const ferrari = new Carro('ferrarO GTB',15,0)
+
+//ferrari.acelerar()
+//ferrari.frear()
+//ferrari.frear()
+//ferrari.exibirVelocidadeAtual()
+
+//if(ferrari.velocidade > fusca.velocidade){
+  //  console.log('A velocidade da ferrari é maior')
+//} else {
+//    console.log('A velocidade do fusca é maior')
+//}
