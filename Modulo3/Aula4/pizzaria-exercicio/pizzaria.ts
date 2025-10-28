dbde0e4d4fa1f6bdd7ffc89d2ae55b7081dc6f3b
lucas <lucasfernandolf@hotmail.com>
2025-10-28 20:37:48 -0300

    pizzaria

---
+++ Modulo3/Aula4/pizzaria-exercicio/pizzaria.ts
@@ -0,0 +1,9 @@
+export class Pizza {
+  constructor(public ingredientes: string[]) {}
+  const = new Pizza(["calabresa", "queijo",]);
+  calculaPreco(){
+    this.ingredientes.length <=2 
+    this.ingredientes.length <=5
+  }
+}
+
