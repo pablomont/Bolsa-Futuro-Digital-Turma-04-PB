352a344f3129a1f48e1decbffb7a0566b9ca60cf
Horlanlacerda <horlansilva56@gmail.com>
2025-10-29 21:40:11 -0300

    Resolução: Questão2 (Módulo3 - Exercício_27_10_25)

---
+++ Modulo3/Exercicios_27_10_25/Exercicio2/index.ts
@@ -0,0 +1,18 @@
+import {Carrinho} from "./carrinho";
+import {Pizza} from "./pizza";
+
+const pizza1 = new Pizza(["Calabresa", "Queijo"]);
+const pizza2 = new Pizza(["Frango", "Catupiry", "Milho"]);
+const pizza3 = new Pizza(["Lombo", "Bacon", "Queijo", "Tomate", "Cebola", "Orégano"]);
+
+const carrinho = new Carrinho();
+
+carrinho.adicionarPizza(pizza1);
+carrinho.adicionarPizza(pizza2);
+carrinho.adicionarPizza(pizza3);
+
+const pizza4 = new Pizza([]);
+
+carrinho.adicionarPizza(pizza4);
+
+console.log(`Total do carrinho: R$ ${carrinho.obtertotal()}`);
