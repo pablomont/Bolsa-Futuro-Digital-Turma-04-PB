2970fa4374cfcbc03fdbd945ae17bec7ad4e1e20
Lethycia Zenaide <lethyciazenaide@gmail.com>
2025-11-01 15:10:49 -0300

    .

---
+++ Modulo3/aula5/gato.ts
@@ -0,0 +1,11 @@
+class Gato{
+    static populacao: number = 0;
+    constructor(public nome: string){
+        Gato.populacao++;
+    }
+}
+
+const gato1 = new Gato('garfield')
+const gato2 = new Gato ('tom')
+
+console.log(Gato.populacao)//vai trasnformar arquivo em typescript
