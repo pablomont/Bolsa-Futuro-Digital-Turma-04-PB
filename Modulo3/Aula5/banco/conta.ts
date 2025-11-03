2586c47adc2ebd75b01944d00fcf1f33a317e655
gabydiniz99 <gabriellasantosdiniz@gmail.com>
2025-11-03 14:03:07 +0000

    banco

--- Modulo3/Aula5/banco/conta.ts
+++ Modulo3/Aula5/banco/conta.ts
@@ -1,42 +1,42 @@
 class ContaCorrente {
+  constructor(public saldo: number) {
+    this.saldo = saldo;
+  }
 
-    constructor(public saldo: number){
-        this.saldo =  saldo;
-    }
-    depositar(valor: number){
-        this.saldo += valor
-    }
-    
-    sacar(valor:number ){
-        if(this.saldo >= valor){
-        this.saldo -= valor
-        console.log('Sucesso - Novo Saldo: ${this.saldo}')
-        }
-        else{
-        console.log('Falha - Saldo insuficiente')
-        }
-    }
+  depositar(valor: number) {
+    this.saldo += valor;
+    console.log('Depósito de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}');
+  }
 
+  sacar(valor: number) {
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+      console.log('Sucesso - Novo Saldo: ${this.saldo}');
+    } else {
+      console.log('Falha - Saldo insuficiente');
+    }
+  }
 }
-
-
 
 class ContaEspecial extends ContaCorrente {
-    limite:number;
-    
-    construtor(saldo: number,
-        limite: number){
-            super.saldo;
-            this.limite = limite;
-        }
-          sacar(valor:number ){
-        if(this.saldo >= valor){
-        this.saldo -= valor
-        }
-    }
+  limite: number;
 
+  constructor(saldo: number, limite: number) {
+    super(saldo);
+    this.limite = limite;
+  }
+
+  sacar(valor: number) {
+    if (this.saldo + this.limite >= valor) {
+      this.saldo -= valor;
+      console.log('Sucesso - Novo Saldo: ${this.saldo}');
+    } else {
+      console.log('Falha - Limite insuficiente');
+    }
+  }
 }
-// const conta Especial = new ContaEspecial(1000)
-// contaEspecial.sacar(800) // sucesso - novo saldo = -300
-// contaEspecial.sacar(800) // falha - saldo insuficiente
-// contaEspecial.depositar(100) // sucesso - novo saldo = -200
+
+const contaEspecial = new ContaEspecial(1000, 500);
+contaEspecial.sacar(1300);
+contaEspecial.sacar(300);
+contaEspecial.depositar(100);
