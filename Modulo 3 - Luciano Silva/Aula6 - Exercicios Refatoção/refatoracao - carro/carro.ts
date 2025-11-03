9733f6d57bd7387cfde97d2f89a26178de33b3f8
LucianoSilva <113153213+I-Zin-I@users.noreply.github.com>
2025-11-03 19:58:32 +0000

    refresh

---
+++ Modulo 3 - Luciano Silva/Aula6 - Exercicios Refatoção/refatoracao - carro/carro.ts
@@ -0,0 +1,52 @@
+import { Painel } from "./painel.js"
+
+class Carro {
+
+    constructor(private nome: string, protected potencia: number, 
+        protected _velocidade :number, public painel: Painel){}
+
+    acelerar(){
+        this.velocidade += this.potencia // velocidade = velocidade + potencia
+    }
+
+    frear(){
+        this.velocidade *= 0.5 // velocidade = velocidade * 0.5
+    }
+
+    get velocidade(){
+        return this._velocidade
+    }
+
+    set velocidade(valor: number){
+        this._velocidade = valor
+
+    }
+}
+
+class CarroEsportivo extends Carro {
+    constructor (nome: string , potencia: number, 
+        velocidade :number, painel: Painel)
+{
+    super(nome, potencia, velocidade, painel)
+}
+
+    acelerar (){
+    this._velocidade += this.potencia * 2
+}
+}
+
+const fusca = new Carro('fusca 1980', 2,0, new Painel())
+const fuscaEsportivo = new CarroEsportivo('fusca 1980',2,0, new Painel())
+
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+//fusca.velocidade = 100
+//fusca.painel.exibir(fusca.velocidade)
+
+fuscaEsportivo.acelerar()
+fuscaEsportivo.acelerar()
+fuscaEsportivo.acelerar()
+
+fusca.painel.exibir(fusca.velocidade)
+fuscaEsportivo.painel.exibir(fuscaEsportivo.velocidade)
