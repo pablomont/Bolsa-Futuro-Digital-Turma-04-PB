01729706a5906d1411b256b7b4d11d843c3ad577
Horlanlacerda <horlansilva56@gmail.com>
2025-11-03 16:46:26 -0300

    Resolução: Questão01 (Exercício_03_11_25)

---
+++ Modulo3/Exercicios_03_11_25/Exercicio1/conta.ts
@@ -0,0 +1,54 @@
+class contaCorrente1{
+
+    constructor(protected _saldo: number = 0){
+        this._saldo = _saldo;
+    }
+
+    protected get saldo(): number{
+        return this._saldo
+    }
+
+    protected set saldo(valor: number){
+        this._saldo = valor
+    }
+
+    depositar(valor: number){
+        this._saldo+= valor
+        return `Sucesso - Novo saldo: ${this._saldo}`
+    }
+
+    sacar(valor:number){
+        if(this._saldo >= valor){
+            this._saldo-=valor
+            return `Sucesso - Novo saldo: ${this._saldo}`
+        }
+    }
+}
+
+class contaEspecial1 extends contaCorrente1{
+    limite: number = 0
+
+    constructor(saldo: number =0, limite:number =0){
+        super(saldo)
+        this.limite = limite
+    }
+
+    sacar(valor:number){
+        if(this.saldo + this.limite >= valor){
+            this.saldo-=valor
+            return `Sucesso - Novo saldo: ${this.saldo}`
+        }
+        else{
+            return "Falha - Saldo insuficiente"
+        }
+    }
+}
+
+const conta3 = new contaCorrente1(100)
+const conta4 = new contaEspecial1(500, 1000)
+
+console.log(conta3.depositar(50))
+console.log(conta3.sacar(50))
+console.log(conta4.sacar(800))
+console.log(conta4.sacar(800))
+console.log(conta4.depositar(100))
