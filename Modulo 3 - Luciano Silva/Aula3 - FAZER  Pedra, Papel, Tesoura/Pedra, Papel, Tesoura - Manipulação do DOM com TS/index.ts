93674b1df798f60a91f321a08072c62b4b564f3e
LucianoSilva <113153213+I-Zin-I@users.noreply.github.com>
2025-11-08 04:16:10 +0000

    Aula3 - Pedra, Papel, Tesoura - inicio

---
+++ Modulo 3 - Luciano Silva/Aula3 - FAZER  Pedra, Papel, Tesoura/Pedra, Papel, Tesoura - Manipulação do DOM com TS/index.ts
@@ -0,0 +1,66 @@
+const btnPedra = document.getElementById('pedra');
+const btnPapel = document.getElementById('papel');
+const btnTesoura = document.getElementById('tesoura');
+
+// ----------------------------------------------
+// Questão 1:
+// Implemente o evento de onclick para os outros botões (papel e tesoura)
+// ----------------------------------------------
+
+if (btnPedra) {
+    btnPedra.onclick = () => {
+        const escolhaComputador = obterEscolhaComputador();
+        mostrarResultado('pedra', escolhaComputador);
+    }
+}
+
+if (btnPapel) {
+    btnPapel.onclick = () => {
+        const escolhaComputador = obterEscolhaComputador();
+        mostrarResultado('papel', escolhaComputador);
+    }
+}
+
+if (btnTesoura) {
+    btnTesoura.onclick = () => {
+        const escolhaComputador = obterEscolhaComputador();
+        mostrarResultado('tesoura', escolhaComputador);
+    }
+}
+
+function obterEscolhaComputador() {
+    const opcoes = ['pedra', 'papel', 'tesoura'];
+    return opcoes[Math.floor(Math.random() * opcoes.length)];
+}
+
+function mostrarResultado(escolhaUsuario: string, escolhaComputador: string) {
+    const opcoes = `Computador: ${escolhaComputador} - Usuário: ${escolhaUsuario}`;
+    
+
+     // ----------------------------------------------
+    // Questão 2:
+    // Termine de implementar a função mostrarResultado
+    // ----------------------------------------------
+
+    if (escolhaUsuario === escolhaComputador) {
+        alert(`É um empate! \n${opcoes}`);
+    } else if(escolhaUsuario === 'papel'){
+        if(escolhaComputador === 'pedra'){
+            alert(`Você venceu! \n${opcoes}`);
+        } else {
+            alert(`O computador venceu! \n${opcoes}`);
+        }
+    } else if(escolhaUsuario === 'pedra'){
+        if(escolhaComputador === 'papel'){
+            alert(`O computador venceu! \n${opcoes}`);
+        } else {
+            alert(`Você venceu! \n${opcoes}`);
+        }
+    } else {
+        if(escolhaComputador === 'pedra'){
+            alert(`O computador venceu! \n${opcoes}`);
+        } else {
+            alert(`Você venceu! \n${opcoes}`);
+        }
+    }
+}
