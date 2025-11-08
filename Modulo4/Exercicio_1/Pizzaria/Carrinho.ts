7ab88eeba4d5b5bb2d8b6082335e6fbd93c35f47
Rodrigues Lima <rodrigues-lima.rl@academico.ifpb.edu.br>
2025-11-08 15:53:16 -0300

    método obterListaPrecos adicionado em Carrinho.ts

---
+++ Modulo4/Exercicio_1/Pizzaria/Carrinho.ts
@@ -0,0 +1,25 @@
+import { Pizza } from "./Pizza.js";
+export class Carrinho {
+    pizzas : Pizza[] = []
+
+    adicionarPizza(pizza:Pizza){
+        if(pizza.ingredientes.length > 0){
+            this.pizzas.push(pizza)
+        } else {
+            console.log("Não é possível adicionar pizza sem ingredientes!")
+        }
+    }
+
+    obterTotal() {
+        let total = 0;
+        for (let pizza of this.pizzas) {
+            total += pizza.preco; 
+        }
+        return total;
+    }
+
+    obterListaPrecos():number[]{
+        return this.pizzas.map(pizza => pizza.preco)
+    }
+
+}
