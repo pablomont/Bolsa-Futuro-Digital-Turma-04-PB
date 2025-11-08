e7ccf0c5153ca5fb9c9746acd5862b1d597a22cf
Mayara Moura <mayarpk07@gmail.com>
2025-11-08 19:47:58 +0000

    map, filter, arrow function

--- Modulo4/aula1/index.js
+++ Modulo4/aula1/index.js
@@ -1,67 +1,53 @@
 // function soma(num1: number, num2: number): number{
 //     return num1+num2;
 // }
-
 // const soma = (num1: number, num2: number): number =>{
 //     return num1+num2;
 // }
-
 // const soma = (num1: number, num2: number) => num1+num2
-
 // soma(1,2)
-
-class Pessoa{
-    constructor(public nome: string, public idade: number){ }
-}
-
-const joao = new Pessoa('Joao', 15)
-const maria = new Pessoa('Maria', 25)
-const jose = new Pessoa('Jose', 30)
-
-const pessoas: Pessoa[] = [joao, maria, jose]
-
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    return Pessoa;
+}());
+var joao = new Pessoa('Joao', 15);
+var maria = new Pessoa('Maria', 25);
+var jose = new Pessoa('Jose', 30);
+var pessoas = [joao, maria, jose];
 // for clássico - C like
 // for(let i= 0; i < pessoas.length; i++){
 //     if(pessoas[i].idade > 18){
 //         console.log('é maior de idade')
 //     }
 // } 
-
- //const idadePessoas: number[] = []
+//const idadePessoas: number[] = []
 // for(let pessoa of pessoas){
 //     idadePessoas.push(pessoa.idade)
 // }
-
 // pessoas.forEach(function(pessoa: Pessoa){
 //     idadePessoas.push(pessoa.idade)
 // })
-
 // pessoas.forEach((pessoa: Pessoa)=>{
 //     idadePessoas.push(pessoa.idade)
 // })
-
 //pessoas.forEach(pessoa => idadePessoas.push(pessoa.idade))
 //console.log(idadePessoas)
-
 // const idadePessoas = pessoas.map(pessoa => pessoa.idade)
 // console.log(idadePessoas)
-
 // const pessoasMaioresDeIdade2 = []
 // for(let pessoa of pessoas){
 //     if(pessoa.idade > 18){
 //         pessoasMaioresDeIdade2.push(pessoa)
 //     }
 // }
-
 // const pessoasMaioresDeIdade = pessoas.filter(pessoa => pessoa.idade > 18)
 // console.log(pessoasMaioresDeIdade)
-
-const nomePessoasMaioresDeIdade = pessoas.filter(pessoa => pessoa.idade > 18).map(pessoa => pessoa.nome)
-console.log(nomePessoasMaioresDeIdade)
-
-
-const luis = new Pessoa('luis', 45)
+var nomePessoasMaioresDeIdade = pessoas.filter(function (pessoa) { return pessoa.idade > 18; }).map(function (pessoa) { return pessoa.nome; });
+console.log(nomePessoasMaioresDeIdade);
+var luis = new Pessoa('luis', 45);
 // const nome = luis.nome
 // const idade = luis.idade
-
-const{nome, idade} = luis
+var nome = luis.nome, idade = luis.idade;
