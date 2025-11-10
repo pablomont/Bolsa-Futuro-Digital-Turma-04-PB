df96c4b32f3840d744c052f3eaa935a334b6d983
Héllida Louize <h.louize@aluno.uepb.edu.br>
2025-11-10 19:59:10 +0000

    my react app

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+
+  return(
+    <>
+   
+    <Gallery/>
+    </>
+  )
+}
+function Profile(){
+  return(
+    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjdmhtYn_BsWh3dHD6oi5fLbnBkKUUy4KLT7VFC0EksDwEYQDSPFT95tdrBjRIaT7YDffV3C5i4Gn-j88Xs7IlZyE-9JwQAkAsk7_1JyCxQ1zltLKsHdoXwYVzkoGfx1LNFVUggvqR6DQbH/s1600/marie-curie-3.jpg" alt=""
+    width="20%" />
+   
+  )
+}
+
+function Gallery(){
+  return(
+    <div>
+      <h1>Cientistas wow</h1>
+      <Profile/>
+      <Profile/>
+      <Profile/>
+    </div>
+  )
+}
+export default App
