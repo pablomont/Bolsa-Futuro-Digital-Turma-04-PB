de507b0706ba78f29baeaf3e40fb98794a58ab58
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-11-13 12:25:58 +0000

    carros-bicicletas

--- Modulo4/aula2/carros-bicicletas/src/App.tsx
+++ Modulo4/aula2/carros-bicicletas/src/App.tsx
@@ -7,7 +7,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h1>Carro e Bicicletas</h1>
+      <h1 className='titulo_centralizado'>Carros e Bicicletas</h1>
       <Carros/>
       <Bicicletas/>
     </div>
@@ -16,16 +16,16 @@ function App() {
 
 function Carros() {
   return (
-    <div>
+    <div className='ver'>
       <h2><span className="TextoBranco">Carro: </span><span className="PrecoBotao">R$ 1999</span></h2>
       <p><span className="Cinza"> Carro modelo</span> <span className="PrecoBotao">2045</span>, <span className="Cinza"> com</span><span className="PrecoBotao"> 200km</span> <span className="Cinza">rodados. </span></p>
       <button> Comprar carro</button>
     </div>
-  )
+  );
 }
 function Bicicletas() {
   return (
-    <div>
+    <div className="ver">
       <h2><span className="TextoBranco">Bicicleta: </span><span className="PrecoBotao"> R$ 199</span></h2>
       <p><span className="Cinza">Bicicleta modelo </span><span className="PrecoBotao">2050</span>, <span className="Cinza">com </span><span className="PrecoBotao">20 km</span><span className="Cinza"> rodados.</span></p>
       <button>Comprar bicicleta</button>
