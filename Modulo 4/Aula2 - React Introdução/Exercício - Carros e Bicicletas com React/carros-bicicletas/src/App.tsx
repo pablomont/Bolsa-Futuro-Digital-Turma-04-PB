1dc13327bac51dfe5c01f2ce9c64aac009dd0ea1
LucianoSilva <113153213+I-Zin-I@users.noreply.github.com>
2025-11-15 07:13:05 +0000

    2 -  M4 - A2 - inicio Carros e bicicletas em reacct

--- Modulo 4/Aula2 - React Introdução/Exercício - Carros e Bicicletas com React/carros-bicicletas/src/App.tsx
+++ Modulo 4/Aula2 - React Introdução/Exercício - Carros e Bicicletas com React/carros-bicicletas/src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+import "./App.css";
+
+  function Carros() {
+    return (
+      <>
+        <h1>ola mundo 2</h1>
+      </>
+    );
+  }
 
 function App() {
-  return(
-  <>
-  <h1>ola mundo</h1>
-  </>
-  )
+  return (
+    <>
+      <h1>ola mundo 1</h1>
+      <Carros />
+    </>
+  );
 }
-export default App
+
+export default App;
