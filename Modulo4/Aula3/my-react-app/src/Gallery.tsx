c61e2d79d1b52b83da52d851020b57ddab6b0f4c
lucas <lucasfernandolf@hotmail.com>
2025-11-17 15:46:23 -0300

    aula3

---
+++ Modulo4/Aula3/my-react-app/src/Gallery.tsx
@@ -0,0 +1,17 @@
+export default function Gallery() {
+  return (
+    <><h1>Hedy Lamarr's Todos</h1>
+    <img 
+        src="https://cdn.britannica.com/95/133095-050-46953AA5/Hedy-Lamarr.jpg" 
+        width="20%"
+        alt="Hedy Lamarr"
+        className="photo"
+    />
+    <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+    </ul>
+    </>
+  );
+}
