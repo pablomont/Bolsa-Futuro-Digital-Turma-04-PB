286e7eb4d12432a066794dc72937779542961248
Pablo Monteiro <paabloms@live.com>
2025-11-17 18:46:36 +0000

    added bio.html

---
+++ Modulo4/aula3/my-react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import TodoList from "./TodoListHedyLamar";
+
+export default function App() {
+  return (
+    <>
+      <TodoList />
+    </>
+  );
+}
+
---
+++ Modulo4/aula3/my-react-app/src/TodoListHedyLamar.tsx
@@ -0,0 +1,18 @@
+export default function TodoListHedyLamar() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img 
+          src="https://cdn.britannica.com/95/133095-050-46953AA5/Hedy-Lamarr.jpg" 
+          width="20%"
+          alt="Hedy Lamarr"
+          className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>  
+    </>
+  );
+}
