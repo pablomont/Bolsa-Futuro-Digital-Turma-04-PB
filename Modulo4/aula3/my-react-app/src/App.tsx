9368a9e3a41e134457445ea30d7720caaa2c50ce
Mayara Moura <mayarpk07@gmail.com>
2025-11-17 19:48:50 +0000

    react

---
+++ Modulo4/aula3/my-react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Gallery from './TodoListHedyLamar'
+
+export default function App() {
+  
+  return (
+    <>
+    <Gallery />
+    </>
+  )
+}
+
---
+++ Modulo4/aula3/my-react-app/src/Profile.tsx
@@ -0,0 +1,7 @@
+export function Profile(){
+  return(
+    <img src="https://s.ebiografia.com/img/ci/en/cientistas_famosos_1.jpg?auto_optimize=low&width=655" 
+    alt="Albert Einstein" 
+    width = '20%' />
+  )
+}
