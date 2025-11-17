5d0ac2bd738a183760a575512a25c03fc53c38ef
Héllida Louize <h.louize@aluno.uepb.edu.br>
2025-11-17 19:03:27 +0000

    hedy!

---
+++ my-react-app/src/Gallery.tsx
@@ -0,0 +1,12 @@
+
+
+export defaut function Gallery(){
+  return(
+    <div>
+      <h1>Cientistas wow</h1>
+      <Profile/>
+      <Profile/>
+      <Profile/>
+    </div>
+  )
+}
---
+++ my-react-app/src/Profile.tsx
@@ -0,0 +1,7 @@
+export function Profile(){
+  return(
+    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjdmhtYn_BsWh3dHD6oi5fLbnBkKUUy4KLT7VFC0EksDwEYQDSPFT95tdrBjRIaT7YDffV3C5i4Gn-j88Xs7IlZyE-9JwQAkAsk7_1JyCxQ1zltLKsHdoXwYVzkoGfx1LNFVUggvqR6DQbH/s1600/marie-curie-3.jpg" alt=""
+    width="20%" />
+   
+  )
+}
