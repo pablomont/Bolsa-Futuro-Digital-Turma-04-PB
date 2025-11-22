26f5263acaf808652a641635e650cb5ebfb02100
LaraFernandesz <lara.marina@aluno.uepb.edu.br>
2025-11-22 16:32:12 -0300

    aula 22.11

---
+++ Modulo3 (TS)/my-react-app/src/utils.ts
@@ -0,0 +1,9 @@
+//podemos colcoar a função em arquivo separado como utils.ts
+export default function getImageUrl(person: any, size: string = 's') {
+    return (
+        'https://i.imgur.com/' +
+        person.imageId +
+        size +
+        '.jpg'
+    );
+}
