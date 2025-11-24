e3c66bc8f059e6bf91e12e67f035cfa692f7c11c
lucas <lucasfernandolf@hotmail.com>
2025-11-24 16:24:03 -0300

    resolve

--- Modulo4/Aula5/packing-list/src/PackingList.tsx
+++ Modulo4/Aula5/packing-list/src/PackingList.tsx
@@ -1,27 +1,34 @@
-function Item({ name, isPacked }: any) {
-let itemContent = name
-if(isPacked){
-    itemContent = name + '✅'
-}
-return <li className="item">{itemContent}</li>
-}
-//     return (
-//         <li className="item">
-//           {isPacked ? name + '✅' : name}
-//         </li>
-//   )
+function Item({ name, isPacked, importance }: any) {
+// let itemContent = name
+// if(isPacked){
+//     itemContent = (
+//         <del>
+//             name + '✅'
+//         </del>
+//      name + '✅'
+// }
+// return <li className="item">{itemContent}</li>
 // }
 
+  return (
+    <li className="item">
+      {isPacked ? name + " ✅" : name + " ❌"}
+      {importance == 0 }
+      ? <i>(Importancia: {importance}) </i>
+    </li>
+  );
+}
+  
 export default function PackingList() {
   return (
     <section>
       <h1>Lista De Bagagem da Sally Ride</h1>
       <ul>
-        <Item isPacked={false} name="Traje Espacial" 
+        <Item isPacked={false} name="Traje Espacial" importance = {9}
         />
-        <Item isPacked={true} name= "Capacete com folha dourada "
+        <Item isPacked={true} name= "Capacete com folha dourada " 
         />
-         <Item isPacked={false} name= " Foto do tam  "
+         <Item isPacked={false} name= " Foto do tam  " importance = {6}
          />
       </ul>
     </section>
