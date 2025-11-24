f1d71114a4640c8fc9c908b018eb1880862197a8
SuelleMaciel <suellemaciel7@gmail.com>
2025-11-24 16:22:27 -0300

    Refatorando Packing-List

--- Questões/Modulo4/Aula5/packing-list/src/PackingList.tsx
+++ Questões/Modulo4/Aula5/packing-list/src/PackingList.tsx
@@ -1,12 +1,21 @@
-function Item({name, isPacked}: any){
+function Item({name, isPacked, importance}: any){
     let itemContent = name
-    if (isPacked){
-        itemContent = name + "✅"
+    let importancia = importance
+   
+    isPacked ? itemContent = <del>{ name + "✅"} </del> : itemContent = name + "❌"; 
+    
+    if (importance < 0) {
+        importancia = null;
     }
     else {
-        itemContent = name + "❌"
+        importancia = importance
     }
-    return <li className="item">{itemContent}</li>
+
+    return ( 
+        <li className="item">
+            {itemContent} {importance > 0 ? <i>(Importância: {importance})</i> : null} 
+        </li>
+    );
 }
 
 export default function PackingList(){
@@ -17,14 +26,17 @@ export default function PackingList(){
                 <Item 
                     isPacked={true}
                     name="Traje Espacial"
+                    importance = {9}
                 />
                 <Item 
                     isPacked={true}
                     name="Capacete com folha dourada"
+                    importance = {0}
                 />
                 <Item 
                     isPacked={false}
                     name="Foto do Tam"
+                    importance = {6}
                 />
             </ul>
         </section>
