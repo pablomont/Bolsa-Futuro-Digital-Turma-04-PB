718852f44d24edcd4150e79a2c8f4fb7dcac90e2
LucianoSilva <113153213+I-Zin-I@users.noreply.github.com>
2025-11-27 01:58:41 +0000

    M4 - A5 - Refatorção  Questão 1:  coffe and tea

--- Modulo 4/Aula5/coffe-and-tea/src/App.tsx
+++ Modulo 4/Aula5/coffe-and-tea/src/App.tsx
@@ -1,40 +1,5 @@
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <DrinkList/>
-    </>
-  )
-}
-
-let info = {
-  part:'',
-  caffeine: '',
-  age:''
-};
-
-function Drink({name}: any) {
-return ( <section>
-<h1>{name}</h1>
-<ul>
-    <li>
-        <strong>Part of plant: </strong>
-        {name ==='tea' ? 'leaf': 'bean'}
-    </li>
-    <li>
-        <strong>Caffeine content: </strong>
-        {name ==='tea' ? '15-70 mg/cup': '80-185 mg/cup'}
-    </li>
-    <li>
-        <strong>Age: </strong>
-        {name === 'tea' ? '4,000+ years' : '1,000+ years'}
-    </li>
-</ul>
-</section>
-);
-}
-
 function DrinkList() {
 return (
   <div>
@@ -44,4 +9,56 @@ return (
 );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <DrinkList/>
+    </>
+  )
+}
+
+function Drink({name}: any) {
+
+let info = {
+  part:'',
+  caffeine: '',
+  age:''
+};
+
+if (name == "tea") {
+  info = {
+    part: "leaf",
+    caffeine:"15-70 mg/cup",
+    age:"4,000 years"
+  };
+
+}else {
+   info = {
+    part: "bean",
+    caffeine: "80-185 mg/cup",
+    age: "1,000 years"
+    };
+  }
+
+return ( 
+<section>
+<h1>{name}</h1>
+<ul>
+    <li>
+        <strong>Part of plant: </strong>
+        {info.part}
+    </li>
+    <li>
+        <strong>Caffeine content: </strong>
+        {info.caffeine}
+    </li>
+    <li>
+        <strong>Age: </strong>
+        {info.age}
+    </li>
+</ul>
+</section>
+);
+}
+
+export default App;
