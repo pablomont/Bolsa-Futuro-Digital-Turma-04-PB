da38aed0cece241bbf29ad595cf434a42d4fa733
Pablo Monteiro <paabloms@live.com>
2025-11-28 20:03:48 +0000

    added ul

---
+++ Modulo4/aula6/list-items/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>Creola Katherine Johnson: mathematician</li>
+        <li>Mario José Molina-Pasquel Henríquez: chemist</li>
+        <li>Mohammad Abdus Salam: physicist</li>
+        <li>Percy Lavon Julian: chemist</li>
+        <li>Subrahmanyan Chandrasekhar: astrophysicist</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
