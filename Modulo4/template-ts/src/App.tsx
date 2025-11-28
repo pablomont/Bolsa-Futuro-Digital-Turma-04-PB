1d2d44fe9f068b3a8b6ca41782f58d540becce26
Pablo Monteiro <paabloms@live.com>
2025-11-28 17:18:56 -0300

    Update App.tsx

--- Modulo4/template-ts/src/App.tsx
+++ Modulo4/template-ts/src/App.tsx
@@ -1,22 +1,15 @@
 import "./App.css";
 
-
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist',
-]
-
 function App() {
-
-  const listItems = people.map(person => <li>{person}</li>)
   return (
     <>
       <ul>
-        
-      </ul>
+      <li>Creola Katherine Johnson: mathematician</li>
+      <li>Mario José Molina-Pasquel Henríquez: chemist</li>
+      <li>Mohammad Abdus Salam: physicist</li>
+      <li>Percy Lavon Julian: chemist</li>
+      <li>Subrahmanyan Chandrasekhar: astrophysicist</li>
+    </ul>
     </>
   )
 }
