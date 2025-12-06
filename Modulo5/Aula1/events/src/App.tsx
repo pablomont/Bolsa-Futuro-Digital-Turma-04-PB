c8862d01fa2e529eef9e03bf56081db5d91aa453
lucas <lucasfernandolf@hotmail.con>
2025-12-06 16:46:28 -0300

    Aula1

--- Modulo5/Aula1/events/src/App.tsx
+++ Modulo5/Aula1/events/src/App.tsx
@@ -1,28 +1,58 @@
-import "./App.css";
+// import "./App.css";
 
-// function App() {
-//   return (
-//     <>
-//       Hello World
-//     </>
-//   )
-// }
-
-export default function Button(){
-
-    // function handleClick(){
-    //   alert('Você Clicou')
-    // }
-
+function Button({ onClick, children }: any) {
   return (
-    <button 
-    onClick={() => alert("Voce clicou ")} 
-    >
-      clique aqui 
+    <button onClick={onClick}>
+      {children}
     </button>
-
-    
   );
-
 }
 
+// function PlayButton({ movieName}: any) {
+//   function handlePlayClick() {
+//     alert(`Playing ${movieName}`);
+//   }
+
+//   return (
+//     <Button onClick={handlePlayClick}>
+//       Play {movieName}
+//     </Button>
+//   );
+// }
+
+// function UploadButton() {
+//   return (
+//     <Button onClick={() => alert("Uploading")}>
+//       Upload Image
+//     </Button>
+//   );
+// }
+
+// export default function ToolBar() {
+//   return (
+//     <>
+//       <PlayButton movieName="Dune Part 2" />
+//       <UploadButton />
+//     </>
+//   );
+// }
+
+export default function ToolBar() {
+  return (
+    <div 
+      className="ToolBar" 
+      onClick={(e) => {
+        e.stopPropagation()
+        alert('Você clicou na div');
+      }}
+    >
+      <button onClick={() => alert('Voce clicou no play')}>
+        Play Movie
+      </button>
+
+      <button onClick={() => alert('Voce clicou no upload')}>
+        Upload Image
+      </button>
+    </div>
+  );
+}
