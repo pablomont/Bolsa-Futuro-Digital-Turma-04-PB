b23bf223a80130718a97e0dc15ff922cd707d0ff
Pablo Monteiro <paabloms@live.com>
2025-12-14 20:53:06 +0000

    added gallery state

--- template-ts/src/App.tsx
+++ template-ts/src/App.tsx
@@ -1,11 +1,32 @@
-import "./App.css";
+import { sculptureList } from './data.ts';
 
-function App() {
+export default function Gallery() {
+  let index = 0;
+
+  function handleClick() {
+    index = index + 1;
+  }
+
+  let sculpture = sculptureList[index];
   return (
     <>
-      Hello World
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
     </>
-  )
+  );
 }
-
-export default App
