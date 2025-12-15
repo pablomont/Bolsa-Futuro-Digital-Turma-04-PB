1f546a9d5037496ff90247b6d48ecec303bd31d0
Mayara Moura <mayarpk07@gmail.com>
2025-12-15 20:02:49 +0000

    react - state

---
+++ Modulo5/aula2/state/src/App.tsx
@@ -0,0 +1,40 @@
+import { sculptureList } from './data.ts';
+import { useState} from 'react';
+
+export default function Gallery() {
+
+  const [index, setIndex] = useState(0)
+  const [showMore, setShowMore] = useState(false)
+
+// index = index + 1 != index + 1
+  function handleClick() {
+    setIndex(index + 1)
+  }
+  function handleMoreClick(){
+    setShowMore(!showMore)
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+        {showMore && <p>{sculpture.description}</p>}
+    </>
+  );
+}
