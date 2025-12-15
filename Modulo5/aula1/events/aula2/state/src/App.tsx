fe8c0034ad6672984178e0aca23d96a52d4fa137
Danilo Gomes <danillogomes1902@gmail.com>
2025-12-15 20:08:11 +0000

    aula2

---
+++ Modulo5/aula1/events/aula2/state/src/App.tsx
@@ -0,0 +1,40 @@
+import { sculptureList } from './data.ts';
+import { useState } from 'react';
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0)
+  const [showMore, setShowmore]= useState(false)
+
+
+  function handleClick() {
+    setIndex (index + 1)
+  }
+
+  function handleShowClick() {
+    setShowmore(!showMore)
+  }
+
+  let sculpture = sculptureList [index];
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <button onClick={handleShowClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      {showMore && <p>{sculpture.description}</p>}
+    </>
+  );
+}
