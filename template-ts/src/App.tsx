f5a2c0753cc38b780c82e042ab02e217e42e2ef5
Pablo Monteiro <paabloms@live.com>
2025-12-19 23:11:27 +0000

    updated template-ts

--- template-ts/src/App.tsx
+++ template-ts/src/App.tsx
@@ -1,32 +1,34 @@
-import { sculptureList } from './data.ts';
+import { useState } from "react";
+import { sculptureList } from "./data";
 
 export default function Gallery() {
-  let index = 0;
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
-  function handleClick() {
-    index = index + 1;
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
   return (
     <>
-      <button onClick={handleClick}>
-        Next
-      </button>
+      <button onClick={handleNextClick}>Next</button>{" "}
       <h2>
-        <i>{sculpture.name} </i> 
+        <i>{sculpture.name} </i>
         by {sculpture.artist}
       </h2>
-      <h3>  
+      <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
-      <img 
-        src={sculpture.url} 
-        alt={sculpture.alt}
-      />
-      <p>
-        {sculpture.description}
-      </p>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
     </>
   );
 }
