e816ccc3d8e5fdac1ecb9e76c19845c09e0644aa
Pablo Monteiro <paabloms@live.com>
2026-01-09 23:39:51 +0000

    changed template-ts

--- template-ts/src/App.tsx
+++ template-ts/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from "react";
-import { sculptureList } from "./data";
+import * as React from 'react';
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
-
-  function handleNextClick() {
-    setIndex(index + 1);
-  }
-
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  let sculpture = sculptureList[index];
+function MyButton({ title, disabled }: any) {
   return (
-    <>
-      <button onClick={handleNextClick}>Next</button>{" "}
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
-    </>
+    <button disabled={disabled}>{title}</button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Bem-vindo ao my app</h1>
+      <MyButton title="Eu sou um botão desabilitado" disabled={true} />
+    </div>
   );
 }
