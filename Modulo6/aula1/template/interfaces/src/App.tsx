8a2f91449189762189f13684d09daed33ca89bb8
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2026-01-10 18:47:40 +0000

    Interfaces para uma classe

---
+++ Modulo6/aula1/template/interfaces/src/App.tsx
@@ -0,0 +1,19 @@
+interface MyButtonProps{
+  title:string
+  disabled: boolean 
+}
+
+function MyButton({title, disabled}: MyButtonProps) {
+  return (
+    <button disabled={disabled}>{title}</button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Bem-vindo ao my app</h1>
+      <MyButton title="Eu sou um botão" disabled={true} />
+    </div>
+  );
+}
