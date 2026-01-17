f6d799bbffdd8903188a0fa471c397d3b1840d64
Héllida Louize <h.louize@aluno.uepb.edu.br>
2026-01-17 18:22:28 +0000

    pure

---
+++ pure/src/App.tsx
@@ -0,0 +1,34 @@
+interface ReceitaProps{
+  pessoas: number;
+}
+
+function Receita({pessoas}: any){
+  return (
+    <div>
+      <ol>
+        <li>
+          Ferva {pessoas} xícaras de água.
+        </li>
+        <li>
+          Adicione {pessoas} colheres de chá e {0.5*pessoas} colheres de especiarias
+        </li>
+        <li>
+          Adicione {pessoas/2} xícaras de leite para ferver com açúcar a gosto
+        </li>
+      </ol>
+    </div>
+  )
+}
+
+export default function App(){
+  return(
+    <section>
+      <h1>Receita de Chá com Especiarias</h1>
+      <h2>Para 2</h2>
+      <Receita pessoas={2}/>
+
+      <h2>Para 4 pessoas</h2>
+      <Receita pessoas={4}/>
+    </section>
+  )
+}
