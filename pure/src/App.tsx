5c67b2dd5f90367c34d24c9513715226f986d320
Héllida Louize <h.louize@aluno.uepb.edu.br>
2026-01-17 18:27:23 +0000

    impure

--- pure/src/App.tsx
+++ pure/src/App.tsx
@@ -1,34 +1,16 @@
-interface ReceitaProps{
-  pessoas: number;
+let convidado = 0
+
+function Xicara(){
+  convidado = convidado +1
+  return <h2>Xícara de chá para o convidado #{convidado}</h2>
 }
 
-function Receita({pessoas}: any){
-  return (
-    <div>
-      <ol>
-        <li>
-          Ferva {pessoas} xícaras de água.
-        </li>
-        <li>
-          Adicione {pessoas} colheres de chá e {0.5*pessoas} colheres de especiarias
-        </li>
-        <li>
-          Adicione {pessoas/2} xícaras de leite para ferver com açúcar a gosto
-        </li>
-      </ol>
-    </div>
-  )
-}
-
-export default function App(){
+export default function ConjuntoDeCha(){
   return(
-    <section>
-      <h1>Receita de Chá com Especiarias</h1>
-      <h2>Para 2</h2>
-      <Receita pessoas={2}/>
-
-      <h2>Para 4 pessoas</h2>
-      <Receita pessoas={4}/>
-    </section>
+    <>
+      <Xicara />
+      <Xicara />
+      <Xicara />
+    </>
   )
 }
